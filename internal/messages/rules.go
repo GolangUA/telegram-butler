889c01878bbf414e7b9b6e78a8522f4a5d603dec
hrvadl <youngwwad@gmail.com>
2024-06-03 11:37:11 +0300

    feat: changed rules msg

--- internal/messages/rules.go
+++ internal/messages/rules.go
@@ -2,27 +2,9 @@ package messages
 
 //nolint:lll
 const Rules = `
-<b>Правила групи</b>:
+<b>Правила групи читай <a href="https://telegra.ph/GolangUA-Rules-06-03">тут</a>.</b>
 
-<b>Заборонено</b>
-- Пропагаднда.
-- Дискримінація.
-- Агресія та образи.
-- Порушення авторських прав.
-- Популяризіція російських ресурсів.
+За порушення правил адміністратори групи можуть дати попередження, відправити в read-only на декілька днів або заблокувати назавжди, <i>в залежності від ситуації</i>. 
 
-<b>Комунікація</b>
-<a href="https://t.me/golang_ua_official/13867"><u>main()</u></a> - основна гілка спілкування, обговорення Go та новин.
-<a href="https://t.me/golang_ua_official/44029"><u>learning</u></a> - допомога при вивченні Go.
-<a href="https://t.me/golang_ua_official/14800"><u>flood</u></a> - флуд :)
-<a href="https://t.me/golang_ua_official/33333"><u>tech (non-Go)</u></a> - обговорення інших технологій 
-<a href="https://t.me/golang_ua_official/37569"><u>interview preparation</u></a> - добірка матеріалів і питань для підготовки до інтервʼю
-<a href="https://t.me/golang_ua_official/14805"><u>vacancy</u></a> - канал для вакансій і пошуку роботи
-<a href="https://t.me/golang_ua_official/19211"><u>kyiv</u></a>, <a href="https://t.me/golang_ua_official/19210"><u>lviv</u></a> - місцеві канали
-<a href="https://t.me/golang_ua_official/15567"><u>algorithms</u></a> - обговорення алгоритмів і вирішення задач по типу leetcode
-<a href="https://t.me/golang_ua_official/14111"><u>переклади</u></a> - канал для перекладів матеріалів
-
-<b>Повний перелік правил читай <a href="https://telegra.ph/GolangUA-rules-06-02">тут</a></b>.
-
-За порушення Code of Conduct адміністратори групи можуть дати попередження, відправити в read-only на декілька днів або заблокувати назавжди, <i>в залежності від ситуації</i>.
+<b>Незнання правил не знімає відповідальності за їх порушення!</b>
 `
