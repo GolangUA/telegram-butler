04fa060cf8fa95739f6ede8aefba28f7d08e5896
hrvadl <youngwwad@gmail.com>
2024-06-10 19:52:25 +0300

    fix: linter errors

--- internal/handler/callback/callbackdata/parser.go
+++ internal/handler/callback/callbackdata/parser.go
@@ -1,6 +1,7 @@
 package callbackdata
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -12,25 +13,32 @@ type Payload struct {
 	MessageID int
 }
 
+var (
+	ErrInvalidCallbackData = errors.New("invalid callback query data token")
+	ErrInvalidGroupID      = errors.New("invalid groupID in callback query data")
+	ErrInvalidMessageID    = errors.New("invalid messageID in callback query data")
+	ErrInvalidDecision     = errors.New("invalid terms of use decision")
+)
+
 func Parse(data string) (*Payload, error) {
 	splits := strings.Split(data, "_")
 	if len(splits) != 3 { //nolint:gomnd,mnd
-		return nil, fmt.Errorf("invalid callback query data token: %v", splits)
+		return nil, fmt.Errorf("%w: %v", ErrInvalidCallbackData, splits)
 	}
 
 	groupID, err := strconv.ParseInt(splits[1], 10, 64)
 	if err != nil {
-		return nil, fmt.Errorf("invalid groupID in callback query data: %s", splits[1])
+		return nil, fmt.Errorf("%w: %s", ErrInvalidGroupID, splits[1])
 	}
 
 	messageID, err := strconv.Atoi(splits[2])
 	if err != nil {
-		return nil, fmt.Errorf("invalid messageID in callback query data: %s", splits[1])
+		return nil, fmt.Errorf("%w: %s", ErrInvalidMessageID, splits[1])
 	}
 
 	decision := splits[0]
 	if decision != AgreeDecision && decision != DeclineDecision {
-		return nil, fmt.Errorf("invalid callback data for terms of use decision: %v", decision)
+		return nil, fmt.Errorf("%w: %v", ErrInvalidDecision, decision)
 	}
 
 	return &Payload{
