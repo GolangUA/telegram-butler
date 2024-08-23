298c38b173ab9f76fa26fcca165af6b08ba43d05
Oleh Baibula <oleh.baibula.ext@ericsson.com>
2024-08-23 09:48:07 +0300

    Refactor logging

--- internal/module/logger/format.go
+++ internal/module/logger/format.go
@@ -6,39 +6,23 @@ import (
 )
 
 func SetupLogger(logLevel string, logFormat string, addSource bool) *slog.Logger {
-	level := StringToLevel(logLevel)
-
-	var handler slog.Handler
-
-	switch logFormat {
-	case "text":
-		// Setup for plain text formatting (assuming you have a handler for that)
-		opts := &slog.HandlerOptions{
-			AddSource: addSource,
-			Level:     level,
-		}
-		handler = slog.NewTextHandler(os.Stdout, opts)
-	case "json":
-		// Setup for JSON formatting
-		handler = slog.NewJSONHandler(
-			os.Stdout,
-			&slog.HandlerOptions{
-				Level:     level,
-				AddSource: addSource,
-			},
-		)
-	case "prettyjson":
-		opts := PrettyHandlerOptions{
-			SlogOpts: slog.HandlerOptions{
-				Level:     StringToLevel(logLevel),
-				AddSource: addSource,
-			},
-		}
-		handler = NewPrettyHandler(os.Stdout, opts)
-	default:
-		// Fallback or default logging format if none specified
-		handler = slog.NewTextHandler(os.Stdout, nil)
+	var level slog.Level
+	err := level.UnmarshalText([]byte(logLevel))
+	if err != nil {
+		level = slog.LevelInfo
 	}
 
-	return slog.New(handler)
+	opts := &slog.HandlerOptions{
+		AddSource: addSource,
+		Level:     level,
+	}
+
+	switch logFormat {
+	case "json":
+		return slog.New(slog.NewJSONHandler(os.Stdout, opts))
+	case "prettyjson":
+		return slog.New(NewPrettyHandler(os.Stdout, opts))
+	default:
+		return slog.New(slog.NewTextHandler(os.Stdout, opts))
+	}
 }
