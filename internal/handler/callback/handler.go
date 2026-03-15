719966bba3a873a0e9b5e956099a7e63b7e7a745
Andrii Derkach <andrsj.derkach@gmail.com>
2026-03-15 22:44:06 +0200

    feat: add 24h read-only restriction for new members (#103)

--- internal/handler/callback/handler.go
+++ internal/handler/callback/handler.go
@@ -3,6 +3,7 @@ package callback
 import (
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
@@ -12,6 +13,7 @@ import (
 	"github.com/GolangUA/telegram-butler/internal/handler/callback/callbackdata"
 	"github.com/GolangUA/telegram-butler/internal/messages"
 	"github.com/GolangUA/telegram-butler/internal/module/logger"
+	"github.com/GolangUA/telegram-butler/internal/module/telegram"
 )
 
 func Register(bh *th.BotHandler) {
@@ -59,6 +61,17 @@ func (h *handler) callbackQuery(ctx *th.Context, query telego.CallbackQuery) err
 		}
 
 		log.Info("Successfully approved join request")
+
+		// 24h read-only for new members
+		restrictErr := ctx.Bot().RestrictChatMember(ctx, &telego.RestrictChatMemberParams{
+			ChatID:      tu.ID(data.GroupID),
+			UserID:      query.From.ID,
+			UntilDate:   time.Now().Add(24 * time.Hour).Unix(),
+			Permissions: telegram.DenyAllPermissions(),
+		})
+		if restrictErr != nil {
+			log.Error("Failed to restrict new member", slog.Any("error", restrictErr))
+		}
 
 		msg = fmt.Sprintf(messages.Welcome, query.From.FirstName, viper.GetString("group-name"))
 	case callbackdata.DeclineDecision:
---
+++ internal/module/telegram/permissions.go
@@ -0,0 +1,19 @@
+package telegram
+
+import "github.com/mymmrac/telego"
+
+// DenyAllPermissions returns ChatPermissions with all send permissions set to false.
+func DenyAllPermissions() telego.ChatPermissions {
+	return telego.ChatPermissions{
+		CanSendMessages:       new(false),
+		CanSendAudios:         new(false),
+		CanSendDocuments:      new(false),
+		CanSendPhotos:         new(false),
+		CanSendVideos:         new(false),
+		CanSendVideoNotes:     new(false),
+		CanSendVoiceNotes:     new(false),
+		CanSendPolls:          new(false),
+		CanSendOtherMessages:  new(false),
+		CanAddWebPagePreviews: new(false),
+	}
+}
